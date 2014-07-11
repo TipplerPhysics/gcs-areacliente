f0284fe59327b88ccbafca7115cfb9096e536aca
martin.gerritsen <martin.gerritsen@gmail.com>
2014-07-11 08:24:45 +0000

    - fixes and more validation

--- GCS/war/js/users.js
+++ GCS/war/js/users.js
@@ -1,27 +1,4 @@
-$.validator.setDefaults({
-	submitHandler: function() {
-		alert("submitted!");
-	},
-	showErrors: function(map, list) {
-		// there's probably a way to simplify this
-		var focussed = document.activeElement;
-		if (focussed && $(focussed).is("input, textarea")) {
-			$(this.currentForm).tooltip("close", {
-				currentTarget: focussed
-			}, true)
-		}
-		this.currentElements.removeAttr("title").removeClass("ui-state-highlight");
-		$.each(list, function(index, error) {
-			$(error.element).attr("title", error.message).addClass("ui-state-highlight");
-		});
-		if (focussed && $(focussed).is("input, textarea")) {
-			$(this.currentForm).tooltip("open", {
-				target: focussed
-			});
-		}
-	}
-});
-
+/* En estas variables guardo la informacion de la linea del grid que se esta editando */
 var cnombre = "";
 var cap1="";
 var cap2="";
@@ -30,28 +7,25 @@ var cpermiso="";
 var areas;
 var dto;
 
-
 function generateRow(data,id,permiso){
-	
 	var a = data.split('&');
-	
 	var nombre = $('#nombre').val();
 	var ap1 = $('#ap1').val();
 	var ap2 = $('#ap2').val();
 	var email = $('#email').val();
-	var dto = $('#dto').val();
-	
-	
+	dto = $('#dto').val();
+
 	var html = "<tr id=row"+id+">"
-	+ "<td><span>"+nombre+"</span></td>"
-	+ "<td><span>"+ap1+"</span></td>"
-	+ "<td><span>"+ap2+"</span></td>"
-	+ "<td><span>"+email+"</span></td>"
-	+ "<td><span>"+permiso+"</span></td>"
-	+ "<td><img class='vs' src='../img/vs.png'>"
-	+ "<a class='papelera' name="+id+"  data-toggle='modal' data-target='#confirm-delete'> </a>"
-	+ "<a class='lapiz' name="+id+"></a></td></tr>" 
-			return html;
+		+ "<td><span>"+nombre+"</span></td>"
+		+ "<td><span>"+ap1+"</span></td>"
+		+ "<td><span>"+ap2+"</span></td>"
+		+ "<td><span>"+email+"</span></td>"
+		+ "<td><span>"+permiso+"</span></td>"
+		+ "<td><img class='vs' src='../img/vs.png'>"
+		+ "<a class='papelera' name="+id+" data-toggle='modal' data-target='#confirm-delete'> </a>"
+		+ "<a class='lapiz' name="+id+"></a></td></tr>";
+	
+	return html;
 }
 
 $('#newUserButton').click(function(e){
@@ -59,147 +33,15 @@ $('#newUserButton').click(function(e){
 		$('#newUserButton').removeClass('white-btn');
 		$('.user_span').removeClass('blue');
 
-		$('.newUser-form').animate({height: "0px"}, 500, function(){
-			$('.newUser-form').css('display','none');
+		$('.new-user-form-holder').animate({height: "0px"}, 500, function(){
+			$('.new-user-form-holder').css('display','none');
 		});
-	}else{
+	} else {
 		$('#newUserButton').addClass('white-btn');
 		$('.user_span').addClass('blue');
-		$('.newUser-form').css('display','inline-block');
-		$('.newUser-form').animate({height: "385px"}, 500);
+		$('.new-user-form-holder').css('display','inline-block');
+		$('.new-user-form-holder').animate({height: "385px"}, 500);
 	}	
-});
-
-$("#user_form").validate({
-		rules: {
-			firstname: "required",
-			lastname: "required",
-			username: {
-				required: true,
-				minlength: 2
-			},
-			password: {
-				required: true,
-				minlength: 5
-			},
-			confirm_password: {
-				required: true,
-				minlength: 5,
-				equalTo: "#password"
-			},
-			email: {
-				required: true,
-				email: true
-			},
-			topic: {
-				required: "#newsletter:checked",
-				minlength: 2
-			},
-			agree: "required"
-		},
-		messages: {
-			firstname: "Please enter your firstname",
-			lastname: "Please enter your lastname",
-			username: {
-				required: "Please enter a username",
-				minlength: "Your username must consist of at least 2 characters"
-			},
-			password: {
-				required: "Please provide a password",
-				minlength: "Your password must be at least 5 characters long"
-			},
-			confirm_password: {
-				required: "Please provide a password",
-				minlength: "Your password must be at least 5 characters long",
-				equalTo: "Please enter the same password as above"
-			},
-			email: "Please enter a valid email address",
-			agree: "Please accept our policy"
-		}
-	});
-
-$("#user_form").submit(function(e) {
-	e.preventDefault(); //STOP default action
-	console.log('hello');
-
-	var areas = "";
-	 if ($('#onboarding')[0].checked==true){
-		 areas += "Onboarding-"
-	 }
-	 if ($('#servicing')[0].checked==true){
-		 areas += "Servicing-"
-	 }
-	 if ($('#itcib')[0].checked==true){
-		 areas += "ITCIB-"
-	 }
-	 if ($('#gcs')[0].checked==true){
-		 areas += "Global Customer Service-"
-	 }
-	 if ($('#global-product')[0].checked==true){
-		 areas += "Global product-"
-	 }
-	 if ($('#clientes')[0].checked==true){
-		 areas += "Clientes-"
-	 }
-	 
-	var servicing = $('#servicing').val();
-	
-    var postData = $(this).serialize() + "&accion=new&areas="+areas;
-    var formURL = $(this).attr("action");
-    console.log('hi');
-    $.ajax(
-    {
-        url : formURL,
-        type: "POST",
-        data : postData,
-        success:function(data, textStatus, jqXHR) 
-        {
-        	alert('succes');
-        	console.log($(data));
-            //data: return data from server
-        	if (data.success==("true")){
-        		var html=generateRow(postData,data.id,data.permiso);
-        		
-        		$('.table_results').prepend(html);
-        		
-        		$('#message_div').removeClass("error");
-        		$('#message_div').addClass("success");
-        		if ($('.newUser-form').height()<190){
-	        		$('.newUser-form').height($('.newUser-form').height()+35);
-        		}
-        		$('#span_message').html("El usuario ha sido creado de forma correcta.")
-        		$('#message_div').css('display','block');
-        		setTimeout(function() { 
-        			$('#user_form')[0].reset();
-        			$( "#message_div" ).fadeOut( "slow", function() {
-        				$('#span_message').html("");
-        		  }); }, 5000);
-        	}else{
-        		$('#message_div').removeClass("success");
-        		$('#message_div').addClass("error");
-        		if ($('.newUser-form').height()<190){
-	        		$('.newUser-form').height($('.newUser-form').height()+35);
-
-        		}
-        		$('#span_message').html(data.error);
-        		$('#message_div').css('display','block');
-        		
-        	}
-        },
-        error: function(jqXHR, textStatus, errorThrown) 
-        {
-        	if (data.failure==("true")){
-        		$('#span_message').html(data.errormessage)
-        		$('#message_div').css('display','block');
-        		
-        		setTimeout(function() { 
-        			$( "#message_div" ).fadeOut( "slow", function() {
-        				$('#span_message').html("");
-        				
-        		  }); }, 5000);
-        	}    
-        }
-    });
 });
 
 $('#confirm-delete').on('show.bs.modal', function(e) {
@@ -207,10 +49,7 @@ $('#confirm-delete').on('show.bs.modal', function(e) {
 });
 
 $('.papelera').on('click', function(e) {
-
-		$('#deleteUser').attr('name',$(this).attr('name'));
-	
-	
+	$('#deleteUser').attr('name',$(this).attr('name'));
 });
 
 $('#deleteUser').on('click', function(e) {
@@ -218,22 +57,19 @@ $('#deleteUser').on('click', function(e) {
 	 var formURL = url + "/usersServlet?";
 	 var postData="accion=delete&id="+ id;
 	 $.ajax(			
-			    {
-			        url : formURL,
-			        type: "POST",
-			        data : postData,
-			        success:function(data, textStatus, jqXHR) 
-			        {
-			        	$('#row'+id).fadeOut("slow");
-			        	$('#confirm-delete').modal('hide');			        	
-			        
-			        }
-			    	
-			    });
+		{
+			url : formURL,
+			type: "POST",
+			data : postData,
+			success:function(data, textStatus, jqXHR) 
+			{
+				$('#row'+id).fadeOut("slow");
+				$('#confirm-delete').modal('hide');			        	
+			}
+		});
 });
 
 $('.lapiz').on('click', function(e) {
-	
 	var id= $(this).attr('name');
 	if ($('.editing')[0] != undefined && !$(this).hasClass('inactive'))
 	{
@@ -241,12 +77,8 @@ $('.lapiz').on('click', function(e) {
 		undoEditRow(rowid);
 		editRow(id);
 	}else if (!$(this).hasClass('inactive')){
-		
 		editRow(id);
-		
-	}
-	
-		
+	}	
 });
 
 $('.table_results').on('click', '.cancelar-ext', function (e) {
@@ -278,12 +110,9 @@ $('.table_results').on('click', '.guardar-ext', function (e) {
 			areas += $item.val() + "-";
 		}		
 	}
-	
-	
-	
+
 	var postData = "&nombre="+nombre+"&id="+id+"&dto="+dto+"&permiso="+permiso+"&email="+email+"&ap1="+ap1+"&ap2="+ap2+"&accion=update&areas="+areas;
     var formURL = "/usersServlet";
-	
 	 $.ajax({
 		        url : formURL,
 		        type: "POST",
@@ -309,13 +138,11 @@ function updateRow(id){
 		$celda.children().remove();
 		$celda.prepend("<span>"+span+"</span>");
 	}
-	
 	var $celda = $(celdas[4]);
 	var span = $celda.children().find(":selected").text();
 	$celda.children().remove();
 	$celda.prepend("<span>"+span+"</span>");
-	
-		        		
+        		
 	$('#lapiz'+id).removeClass('inactive');
 	$('#papelera'+id).removeClass('inactive');
 }
@@ -331,8 +158,7 @@ function undoRow(id,arr){
 		$celda.children().remove();
 		$celda.prepend("<span>"+arr[a]+"</span>");
 	}
-	
-	
+
 /*	$('#return'+id).css('display','none');
 	$('#guardar'+id).css('display','none');
 	$('#papelera'+id).css('display','inline-block');
@@ -350,9 +176,6 @@ function undoEditRow(id){
 	$('.extended-row').remove();
 	
 	updateRow(id);
-	
-	
-	
 }
 
 function generateRow(pagina,destino){
@@ -375,7 +198,6 @@ function generateRow(pagina,destino){
 		}
 	},'html');
 }
-
 
 function editRow(id){
 	$('#papelera'+id).removeAttr('data-toggle');
@@ -475,8 +297,144 @@ function editRow(id){
 			$('#e-'+name)[0].setAttribute("checked", "checked");
 		}
 	}},500);
-	
-	
-	
 }
 
+$(function() {
+	// Submit for creating a new user.
+	$("#new-user-form").submit(function(e) {
+		e.preventDefault(); //STOP default action
+
+		var areas = "";
+		 if ($('#onboarding')[0].checked==true){
+			 areas += "Onboarding-"
+		 }
+		 if ($('#servicing')[0].checked==true){
+			 areas += "Servicing-"
+		 }
+		 if ($('#itcib')[0].checked==true){
+			 areas += "ITCIB-"
+		 }
+		 if ($('#gcs')[0].checked==true){
+			 areas += "Global Customer Service-"
+		 }
+		 if ($('#global-product')[0].checked==true){
+			 areas += "Global product-"
+		 }
+		 if ($('#clientes')[0].checked==true){
+			 areas += "Clientes-"
+		 }
+		 
+		var servicing = $('#servicing').val();
+		
+	    var postData = $(this).serialize() + "&accion=new&areas="+areas;
+	    var formURL = $(this).attr("action");
+	    $.ajax(
+	    {
+	        url : formURL,
+	        type: "POST",
+	        data : postData,
+	        success:function(data, textStatus, jqXHR) 
+	        {
+	        	alert('succes');
+	        	console.log($(data));
+	            //data: return data from server
+	        	if (data.success==("true")){
+	        		var html=generateRow(postData,data.id,data.permiso);
+	        		
+	        		$('.table_results').prepend(html);
+	        		
+	        		$('#message_div').removeClass("error");
+	        		$('#message_div').addClass("success");
+	        		if ($('.new-user-form-holder').height()<190){
+		        		$('.new-user-form-holder').height($('.new-user-form-holder').height()+35);
+	        		}
+	        		$('#span_message').html("El usuario ha sido creado de forma correcta.")
+	        		$('#message_div').css('display','block');
+	        		setTimeout(function() { 
+	        			$('#user_form')[0].reset();
+	        			$( "#message_div" ).fadeOut( "slow", function() {
+	        				$('#span_message').html("");
+	        		  }); }, 5000);
+	        	}else{
+	        		$('#message_div').removeClass("success");
+	        		$('#message_div').addClass("error");
+	        		if ($('.new-user-form-holder').height()<190){
+		        		$('.new-user-form-holder').height($('.new-user-form-holder').height()+35);
+
+	        		}
+	        		$('#span_message').html(data.error);
+	        		$('#message_div').css('display','block');
+	        		
+	        	}
+	        },
+	        error: function(jqXHR, textStatus, errorThrown) 
+	        {
+	        	if (data.failure==("true")){
+	        		$('#span_message').html(data.errormessage)
+	        		$('#message_div').css('display','block');
+	        		
+	        		setTimeout(function() { 
+	        			$( "#message_div" ).fadeOut( "slow", function() {
+	        				$('#span_message').html("");
+	        				
+	        		  }); }, 5000);
+	        	}    
+	        }
+	    });
+	});
+	// Setup form validation on the #register-form element
+	$('form').validate({
+
+	    // Specify the validation rules
+	    rules: {
+	        email: {
+	            required: true,
+	            email: true
+	        },
+	        password: {
+	            required: true,
+	            minlength: 5
+	        },
+	        agree: "required"
+	    },
+	    
+	    // Specify the validation error messages
+	    messages: {
+	        firstname: "Please enter your first name",
+	        lastname: "Please enter your last name",
+	        password: {
+	            required: "Please provide a password",
+	            minlength: "Your password must be at least 5 characters long"
+	        },
+	        email: "Please enter a valid email address",
+	        agree: "Please accept our policy"
+	    },
+	    
+	    submitHandler: function(form) {
+	        form.submit();
+	    }
+	});
+
+	$.extend($.validator.messages, {
+		required: "Este campo es obligatorio.",
+		remote: "Por favor, rellena este campo.",
+		email: "Por favor, escribe una dirección de correo válida.",
+		url: "Por favor, escribe una URL válida.",
+		date: "Por favor, escribe una fecha válida.",
+		dateISO: "Por favor, escribe una fecha (ISO) válida.",
+		number: "Por favor, escribe un número válido.",
+		digits: "Por favor, escribe sólo dígitos.",
+		creditcard: "Por favor, escribe un número de tarjeta válido.",
+		equalTo: "Por favor, escribe el mismo valor de nuevo.",
+		extension: "Por favor, escribe un valor con una extensión aceptada.",
+		maxlength: $.validator.format("Por favor, no escribas más de {0} caracteres."),
+		minlength: $.validator.format("Por favor, no escribas menos de {0} caracteres."),
+		rangelength: $.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
+		range: $.validator.format("Por favor, escribe un valor entre {0} y {1}."),
+		max: $.validator.format("Por favor, escribe un valor menor o igual a {0}."),
+		min: $.validator.format("Por favor, escribe un valor mayor o igual a {0}."),
+		nifES: "Por favor, escribe un NIF válido.",
+		nieES: "Por favor, escribe un NIE válido.",
+		cifES: "Por favor, escribe un CIF válido."
+	});
+});
