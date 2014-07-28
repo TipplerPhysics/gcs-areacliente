2a4e801536025dfcad0ff0bde092953daa23e87f
martingerritsen@gmail.com <martingerritsen@gmail.com@b040bd45-dec4-fd90-964e-ca82f1cfcec5>
2014-07-28 16:09:15 +0000

    revision grabar demanda

--- trunk/GCS/src/com/gcs/dao/DemandaDao.java
+++ trunk/GCS/src/com/gcs/dao/DemandaDao.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import javax.jdo.PersistenceManager;
 
-import com.gcs.beans.ContadorDemanda;
 import com.gcs.beans.Demanda;
-import com.gcs.beans.User;
 import com.gcs.persistence.PMF;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
@@ -38,16 +36,11 @@ DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
    }
 	
 	public void  createDemanda(Demanda d){
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		ContadorDemandaDao cdDao = ContadorDemandaDao.getInstance();
-		
+		PersistenceManager pm = PMF.get().getPersistenceManager();		
 		try{
 			pm.makePersistent(d);
-			
-			
 		}finally{
 			pm.close();
-			cdDao.increaseCont();
 		}
 	}
 
@@ -62,24 +55,5 @@ DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		return demandas;
 	}
 	
-	@SuppressWarnings("unchecked")
-	public String countDemandas(){
-		
-		ContadorDemandaDao cdDao = ContadorDemandaDao.getInstance();
-		Integer a = cdDao.getContador().getNum();
-		
-		String str = a.toString();
-		
-		if (str.length()<=1){
-			str = "000"+str;
-		}else if (str.length()<=2){
-			str = "00"+str;
-		}else if (str.length()<=3){
-			str = "0"+str;
-		}
-		
-		return str;
-		
-		
-	}
+	
 }
