400fb72067d9330cde2ff20de3e80766db18af93
david.martin.beltran.contractor@bbva.com <david.martin.beltran.contractor@bbva.com@b040bd45-dec4-fd90-964e-ca82f1cfcec5>
2014-09-09 18:01:34 +0000

    edit demanda new

--- trunk/GCS/war/js/dev/scripts.js
+++ trunk/GCS/war/js/dev/scripts.js
@@ -556,7 +556,7 @@ $(function() {
 		var id= $(this).attr('name');
 		if (!$(this).hasClass('inactive')) {
 			editRowDemanda(id);
-			disableSearch();
+			
 
 		}	
 	});
@@ -724,101 +724,8 @@ $(function() {
 		var fechaComun = $currentRow.data('fecha-comun');
 		var horaComun = $currentRow.data('hora-comun');
 
-		$.get('../html/extended-demanda.html',null,function(result) {
-			// Close other editing field and show the row.
-			if($previousOpenEdit.length > 0){
-				$('#row'+$previousOpenEdit.data('row-id')).css({display:'table-row'});
-				$previousOpenEdit.remove();
-			}
-			// Hide current row.
-			$currentRow.css({display:'none'});
-			// Adds the item holder row for editing the item.
-			$currentRow.after("<tr id='edit-item-holder' class='extended-row' style='display: table-row;'><td colspan='6'><div class='extended-div'></div></td></tr>");
-			var $currentOpenEdit = $table.find('#edit-item-holder');
-			$currentOpenEdit.data('row-id', id);
-			// Add the result to the element
-			$(".extended-div").html(result);
-			// The form we're editing in.
-			var $editForm = $currentOpenEdit.find('form#edit-item');
-			// Add info stuff ... errr ... 0_o.
-			$editForm.find('#fecha_entrada_peticion_ed').val(fechaEntrada);
-			// copia options de select de formulario de creacion
-			var $clienteOptions = $('select#input_cliente option').clone();
-			$editForm.find('select#input_cliente_ed').append($clienteOptions);
-			var inputvar = $('#input_cliente_ed').children();
-			var clientValue;
-			for (var a=0; a<=inputvar.length-1;a++)
-				if (inputvar[a].innerHTML==cliente)
-					clientValue = inputvar[a].value;
+		
 			
-			$editForm.find('select#input_cliente_ed').val(clientValue);
-			
-			$editForm.find('select#input_cliente_ed option').first().remove();
-
-			var $tipoOptions = $('select#tipo option').clone();
-			$editForm.find('select#input_tipo_ed').append($tipoOptions).val(tipo);
-			$editForm.find('select#input_tipo_ed option').first().remove();
-
-			var $tipoOptions = $('select#estado option').clone();
-			$editForm.find('select#input_estado_ed').append($tipoOptions).val(estado);
-			$editForm.find('select#input_estado_ed option').first().remove();
-
-			$editForm.find('#cod_peticion_ed').html(codPeticion);
-
-			var $gestorOptions = $('select#gestor_it option').clone();
-			$editForm.find('select#gestor_it_ed').append($gestorOptions).val(gestorAsignado);
-			$editForm.find('select#gestor_it_ed option').first().remove();
-
-			// If it has a fecha it might have a time too.
-			if(fechaComun.length > 0) {
-				$('#fecha_solicitud_asignacion_ed').val(fechaComun);
-				$editForm.find('select#hora_peticion_ed').val(horaComun.substring(0, 2));
-				$editForm.find('select#min_peticion_ed').val(horaComun.substring(3, 5));
-			}
-
-			// Activate everything.
-			initDatepickers();
-			initSelectpickers();
-			initForms();
-			initValidator();
-			// Click event for the cancel button.
-			$editForm.on('click', '.cancelar-ext', function (e) {
-				$('#row'+$currentOpenEdit.data('row-id')).css({display:'table-row'});
-				$currentOpenEdit.remove();
-				enableSearch();
-				return false;
-			});
-			// Click event for the save button.
-			$editForm.on('click', '.guardar-ext', function (e) {
-				if($editForm.valid()){
-					var $editItemHolder = $(this).closest('#edit-item-holder');
-					// Collect all the information.
-					var id= $editItemHolder.data('row-id');
-					var fecha_entrada_peticion = $editItemHolder.find('input#fecha_entrada_peticion_ed').val();
-					var input_cliente = $('#input_cliente_ed option:selected').val();
-					var input_tipo = $('#input_tipo_ed option:selected').val();
-					var input_estado = $('#input_estado_ed option:selected').val();
-					var gestor_it = $('#gestor_it_ed option:selected').val();
-					var fecha_solicitud_asignacion = $editItemHolder.find('input#fecha_solicitud_asignacion_ed').val();
-					var hora_peticion = $('#hora_peticion_ed option:selected').val();
-					var min_peticion = $('#min_peticion_ed option:selected').val();
-					
-					var postData = "fecha_entrada="+fecha_entrada_peticion+"&cliente="+input_cliente+"&tipo="+input_tipo+"&estado="+input_estado+"&gestor_it="+gestor_it+"&fecha_asignacion="+fecha_solicitud_asignacion+"&hora_peticion_ext="+hora_peticion+"&accion=update&min_peticion_ext="+min_peticion+"&id="+id;
-					var formURL = "/demandaServlet";
-
-					$.ajax({
-						url : formURL,
-						type: "POST",
-						data : postData,
-						success:function(data, textStatus, jqXHR) {
-							enableSearch();
-							location.reload();
-						}
-					});
-				}
-				return false;
-			});
-		});
 	}
 	
 $("#submit_demanda_form").on('click',function(e) {
@@ -1704,36 +1611,42 @@ function drawChecksAreas(str){
 			$('#onboarding_modal').next().addClass("checked");
 		}else{
 			$('#onboarding_modal').removeClass("checked");
+			$('#onboarding_modal').next().removeClass("checked");
 		}		
 		if (str[x].indexOf("servicing")!=-1){
 			$('#servicing_modal').attr("checked","checked");
 			$('#servicing_modal').next().addClass("checked");
 		}else{
 			$('#servicing_modal').removeClass("checked");
+			$('#servicing_modal').next().removeClass("checked");
 		}				
 		if (str[x].indexOf("clientes")!=-1){
 			$('#clientes_modal').attr("checked","checked");
 			$('#clientes_modal').next().addClass("checked");
 		}else{
 			$('#clientes_modal').removeClass("checked");
+			$('#clientes_modal').next().removeClass("checked");
 		}			
 		if (str[x].indexOf("itcib")!=-1){
 			$('#itcib_modal').attr("checked","checked");
 			$('#itcib_modal').next().addClass("checked");
 		}else{
 			$('#itcib_modal').removeClass("checked");
+			$('#itcib_modal').next().removeClass("checked");
 		}
 		if (str[x].indexOf("gcs")!=-1){
 			$('#gcs_modal').attr("checked","checked");
 			$('#gcs_modal').next().addClass("checked");
 		}else{
 			$('#gcs_modal').removeClass("checked");
+			$('#gcs_modal').next().removeClass("checked");
 		}
 		if (str[x].indexOf("globalproduct")!=-1){
 			$('#global-product_modal').attr("checked","checked");
 			$('#global-product_modal').next().addClass("checked");
 		}else{
 			$('#global-product_modal').removeClass("checked");
+			$('#global-product_modal').next().removeClass("checked");
 		}
 	}
 }
