b6f305a813fb6abbb65484f630c4bd4aa6f45019
david.martin.beltran.contractor@bbva.com <david.martin.beltran.contractor@bbva.com@b040bd45-dec4-fd90-964e-ca82f1cfcec5>
2014-12-10 14:27:35 +0000

    persistent capable flag

--- trunk/GCS/src/com/gcs/beans/Implantacion.java
+++ trunk/GCS/src/com/gcs/beans/Implantacion.java
@@ -3,11 +3,13 @@ package com.gcs.beans;
 import java.util.Date;
 
 import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
 import com.google.appengine.api.datastore.Key;
 
+@PersistenceCapable
 public class Implantacion {
 	
 	
