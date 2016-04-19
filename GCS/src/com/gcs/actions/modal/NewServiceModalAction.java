4bb9d65d5e71c3fd8557c56cad39e8571a0ccb0b
fmexposito <fexposit@LES000892.corp.capgemini.com>
2016-04-19 12:04:26 +0100

    Nuevos campos en alta servicio desde Gestión de Proyectos

--- GCS/src/com/gcs/actions/modal/NewServiceModalAction.java
+++ GCS/src/com/gcs/actions/modal/NewServiceModalAction.java
@@ -1,6 +1,7 @@
 package com.gcs.actions.modal;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,15 +13,19 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import com.gcs.beans.Estados;
+import com.gcs.beans.Formatos;
 import com.gcs.beans.Pais;
 import com.gcs.beans.Proyecto;
 import com.gcs.beans.Servicio;
 import com.gcs.beans.ServicioFile;
+import com.gcs.beans.User;
 import com.gcs.dao.EstadosDao;
+import com.gcs.dao.FormatosDao;
 import com.gcs.dao.PaisDao;
 import com.gcs.dao.ProyectoDao;
 import com.gcs.dao.ServicioDao;
 import com.gcs.dao.ServicioFileDao;
+import com.gcs.dao.UserDao;
 
 public class NewServiceModalAction extends Action {
 	
@@ -42,12 +47,23 @@ public class NewServiceModalAction extends Action {
 		
 		EstadosDao estDao = EstadosDao.getInstance();
 		List<Estados> estados = estDao.getAllEstados();
+	
+		// Obtengo lista de Gestores para rellenar la select
+		UserDao uDao = UserDao.getInstance();		
+		List<User> gestores_it = uDao.getUsersByPermisoStr(3);
+
+		FormatosDao formatosDao = FormatosDao.getInstance();
+		List<Formatos> formatos = formatosDao.getAllFormatos();
+		req.setAttribute("formatos", formatos);
+
 		
 		req.setAttribute("proyectos", proyectos);
 		req.setAttribute("servicios", servicios);
 		req.setAttribute("serviciosFile", serviciosFile);
 		req.setAttribute("paises", paises);
 		req.setAttribute("estados", estados);
+		req.setAttribute("gestores_it", gestores_it);
+
 
 
 		return mapping.findForward("ok");
